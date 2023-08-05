4753ba7039649a38970ad45144b3fb1980a3d485
dannykoes <sandal2610@gmail.com>
2023-08-05 10:25:25 +0700

    update

--- resources/views/backend/beranda.blade.php
+++ resources/views/backend/beranda.blade.php
@@ -94,7 +94,7 @@
                             <div class="btn-group  mb-2 me-4" role="group">
                                 <button id="btndefault" type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgb(240, 240, 240);transform: ;msFilter:;"><path d="m2.344 15.271 2 3.46a1 1 0 0 0 1.366.365l1.396-.806c.58.457 1.221.832 1.895 1.112V21a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-1.598a8.094 8.094 0 0 0 1.895-1.112l1.396.806c.477.275 1.091.11 1.366-.365l2-3.46a1.004 1.004 0 0 0-.365-1.366l-1.372-.793a7.683 7.683 0 0 0-.002-2.224l1.372-.793c.476-.275.641-.89.365-1.366l-2-3.46a1 1 0 0 0-1.366-.365l-1.396.806A8.034 8.034 0 0 0 15 4.598V3a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v1.598A8.094 8.094 0 0 0 7.105 5.71L5.71 4.904a.999.999 0 0 0-1.366.365l-2 3.46a1.004 1.004 0 0 0 .365 1.366l1.372.793a7.683 7.683 0 0 0 0 2.224l-1.372.793c-.476.275-.641.89-.365 1.366zM12 8c2.206 0 4 1.794 4 4s-1.794 4-4 4-4-1.794-4-4 1.794-4 4-4z"></path></svg></button>
                                 <div class="dropdown-menu" aria-labelledby="btndefault">
-                                    <a href="javascript:void(0);" class="dropdown-item" data-tugas="{{$f->tugas}}" onclick="edit($(this))"><i class="flaticon-home-fill-1 mr-1"></i>Edit</a>
+                                    <a href="javascript:void(0);" class="dropdown-item" onclick="edit({{$f}})"><i class="flaticon-home-fill-1 mr-1"></i>Edit</a>
                                 </div>
                             </div>
                         </td>
@@ -524,18 +524,19 @@
         let no = 0;
         let json = false;
         let foto = false;
-        if (data.attr('data-tugas')) {
-            json = JSON.parse(data.attr('data-tugas'));
+        if (data.tugas) {
+            json = JSON.parse(data.tugas);
         }
-        // if (data.tugas) {
-        //     foto = JSON.parse(data.foto);
-        // }
+        if (data.tugas) {
+            foto = JSON.parse(data.foto);
+        }
+        console.log(data);
         console.log(json,foto);
         $('#detailjob').modal('show');
         $('#namapetugas').html(data.name);
         $('#tugasid').html(data.id);
         $('#detaildata').val(data.tugas);
-        json.forEach(e => {
+        // json.forEach(e => {
         // a+='<div class="row">';
         // a+='    <div class="col"><small>Kantor</small><h5>'+e.namakantor+'</h5></div>';
         // a+='    <div class="col"><small>Lantai</small><h5>'+e.lantai+'</h5></div>';
@@ -554,32 +555,32 @@
         // a+='    </div>';
         // a+='</div>';
 
-        a+='<tr>';
-        a+='    <td>'+e.namakantor+'</td>';
-        a+='    <td>'+e.lantai+'</td>';
-        a+='    <td>'+e.ruangan+'</td>';
-        a+='    <td>'+e.nama_tugas+'</td>';
-        a+='    <td>';
-        a+='        <a href="'+foto?foto[no].url:''+'" target="_blank"><img src="'+foto?foto[no].url:''+'" alt="" width="60px" height="60px"></a>';
-        a+='    </td>';
-        a+='    <th><textarea name="detailkomentar[]" id="detailkomentar" cols="30" rows="1" class="form-control" placeholder="Komentar"></textarea></th>';
-        a+='    <th>';
-        a+='        <select name="detailnilai[]" id="detailnilai" class="form-control">';
-        a+='            <option value="">Pilih</option>';
-        a+='            <option value="5">Bersih Sekali</option>';
-        a+='            <option value="4">Bersih</option>';
-        a+='            <option value="3">Cukup</option>';
-        a+='            <option value="2">Kurang Bersih</option>';
-        a+='            <option value="1">Kotor</option>';
-        a+='        </select>';
-        a+='    </th>';
-        a+='</tr>';
-        no++;
-        });
+        // a+='<tr>';
+        // a+='    <td>'+e.namakantor+'</td>';
+        // a+='    <td>'+e.lantai+'</td>';
+        // a+='    <td>'+e.ruangan+'</td>';
+        // a+='    <td>'+e.nama_tugas+'</td>';
+        // a+='    <td>';
+        // a+='        <a href="'+foto?foto[no].url:''+'" target="_blank"><img src="'+foto?foto[no].url:''+'" alt="" width="60px" height="60px"></a>';
+        // a+='    </td>';
+        // a+='    <th><textarea name="detailkomentar[]" id="detailkomentar" cols="30" rows="1" class="form-control" placeholder="Komentar"></textarea></th>';
+        // a+='    <th>';
+        // a+='        <select name="detailnilai[]" id="detailnilai" class="form-control">';
+        // a+='            <option value="">Pilih</option>';
+        // a+='            <option value="5">Bersih Sekali</option>';
+        // a+='            <option value="4">Bersih</option>';
+        // a+='            <option value="3">Cukup</option>';
+        // a+='            <option value="2">Kurang Bersih</option>';
+        // a+='            <option value="1">Kotor</option>';
+        // a+='        </select>';
+        // a+='    </th>';
+        // a+='</tr>';
+        // no++;
+        // });
         $('#detail').html(a);
     }
     $(document).ready(function () {
-        console.log(09.53);
+        console.log(09.5);
     })
 </script>
 
