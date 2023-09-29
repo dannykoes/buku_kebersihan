36fd2a1807e539f4f46afbdac0351ec98e675a43
dannykoes <sandal2610@gmail.com>
2023-09-29 14:22:18 +0700

    t

--- database/migrations/2023_09_29_105643_create_a_outdoor_models_table.php
+++ database/migrations/2023_09_29_105643_create_a_outdoor_models_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // 
         Schema::create('a_outdoor_models', function (Blueprint $table) {
             $table->id();
             $table->integer('kantor_id');
